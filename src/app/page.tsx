ab07e843da2c539fa3bf2ee5f8a035144629a94e
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-09 16:22:38 -0400

    add components and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import AddTodo from '@/components/shared/AddTodo';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <span className="text-3xl font-bold uppercase">To-Do App</span>
+      <h1 className="text-3xl font-extrabold uppercase mb-5 ">
+        Next.js 14
+        <span className="text-cyan-800 ml-2">Server Actions</span>
+        <div className="flex justify-center flex-col items-center w-[1000px]">
+          <AddTodo />
+        </div>
+      </h1>
+    </div>
+  );
 }
