d1552548003e0cbd57602321ac6ffbf234851a84
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-10 12:32:24 -0400

    add changeTodo component and action

--- src/components/shared/ToDo.tsx
+++ src/components/shared/ToDo.tsx
@@ -1,8 +1,18 @@
 import { Todo } from '@prisma/client';
+import ChangeTodo from './ChangeTodo';
+import { todoType } from '@/types/todoType';
 
-const ToDo = ({ todo }: { todo: Todo }) => {
+const ToDo = ({ todo }: { todo: todoType }) => {
+  const todoStyle = {
+    textDecoration: todo.isCompleted ? 'line-through' : 'none',
+    opacity: todo.isCompleted ? 0.5 : 1,
+  };
   return (
-    <div className="w-full flex items-center justify-between bg-white py-3 px-20 rounded-2xl">
+    <div
+      style={todoStyle}
+      className="w-full flex items-center justify-between bg-white py-3 px-20 rounded-2xl"
+    >
+      <ChangeTodo todo={todo} />
       <span className="text-center font-bold uppercase">{todo.title}</span>
     </div>
   );
